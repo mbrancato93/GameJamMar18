e82d6b2e24e9597026c2d98c8f0fd446a3c9136f
Mike <michael.brancato93@gmail.com>
2022-03-20 12:06:48 -0400

    Update - MB

---
+++ core/objects/Portal.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+var repeats := 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.connect( "animation_finished", self, "portal_done" )
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func portal_done( _val = null ):
+	if( repeats ):
+		$AnimationPlayer.play( "trigger" )
+		repeats -= 1
+		return
+	$AnimationPlayer.disconnect( "animation_finished", self, "transition_end" )
+	get_parent().reset()
+	pass
+
+
+func _on_Portal_body_entered(body):
+	if( body.name == "Player" && body.state != 1 ):
+		body.freeze = true
+		$AnimationPlayer.play( "trigger" )
+	pass # Replace with function body.
